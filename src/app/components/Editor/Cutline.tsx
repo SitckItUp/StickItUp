b25251d6a4e8c07582928d3eada53f87539e7b89
James <james101994@yahoo.com>
2023-08-14 16:41:28 -0700

    Write function to expand SVG sizing based on multiplicative factor, needs styling fix to re-center SVG in respsect to original image

--- src/app/components/Editor/Cutline.tsx
+++ src/app/components/Editor/Cutline.tsx
@@ -1,12 +1,15 @@
-import { useState } from "react"
+import { useState } from "react";
 
 interface CutlineSettings {
   [key: string]: {
     text: string;
     thumbnail: string;
-  }
+  };
 }
 
+interface CutlineProps {
+  expandCutLine: React.Dispatch<React.SetStateAction<string | null>>;
+}
 
 // Cutline Settings
 const cutlineSettings: CutlineSettings = {
@@ -22,47 +25,52 @@ const cutlineSettings: CutlineSettings = {
     text: "Large",
     thumbnail: "large.png",
   },
-  square: {
-    text: "Square",
-    thumbnail: "square.png",
-  },
-  circle: {
-    text: "Circle",
-    thumbnail: "circle.png",
-  },
-  rounded: {
-    text: "Rounded",
-    thumbnail: "rounded.png",
-  },
-}
+  // square: {
+  //   text: "Square",
+  //   thumbnail: "square.png",
+  // },
+  // circle: {
+  //   text: "Circle",
+  //   thumbnail: "circle.png",
+  // },
+  // rounded: {
+  //   text: "Rounded",
+  //   thumbnail: "rounded.png",
+  // },
+};
 
-export default function Cutline () {
-  const [activeIcon, setActiveIcon] = useState<string>()
+export default function Cutline({ expandCutLine }: CutlineProps) {
+  const [activeIcon, setActiveIcon] = useState<string>();
 
   const handleIconClick = (el: string) => {
-    setActiveIcon(el)
-  }
+    expandCutLine(el);
+    setActiveIcon(el);
+  };
 
-  const options: React.ReactNode[] = Object.keys(cutlineSettings).map((el: string) => {
-    return (
-    <div 
-      key={el} 
-      className={`hover:bg-slate-200 rounded hover:cursor-pointer ${activeIcon === el ? "bg-gray-300 py-2" : "hover:bg-gray-300 py-2"}}`}
-      onClick={() => {handleIconClick(el)}}
-    >
-      {cutlineSettings[el].text}
-    </div>
-    )
-  })
+  const options: React.ReactNode[] = Object.keys(cutlineSettings).map(
+    (el: string) => {
+      return (
+        <div
+          key={el}
+          className={`hover:bg-slate-200 rounded hover:cursor-pointer ${
+            activeIcon === el ? "bg-gray-300 py-2" : "hover:bg-gray-300 py-2"
+          }}`}
+          onClick={() => {
+            handleIconClick(el);
+          }}
+        >
+          {cutlineSettings[el].text}
+        </div>
+      );
+    }
+  );
 
   return (
     <div className="outline-tool-container flex justify-center self-start bg-slate-100 h-1/6 w-full">
       <div>
         <h1 className="text-2xl font-bold">Cutline</h1>
-        <div className="grid grid-cols-3 gap-4">
-          {options}
-        </div>
+        <div className="grid grid-cols-3 gap-4">{options}</div>
       </div>
     </div>
-  )
+  );
 }
